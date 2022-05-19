2a21f6d85f15f8f2dd19c8d78c98044d2b7e0523
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2022-05-19 17:43:57 -0600

    Create Numero_recursivo.py

---
+++ Numero_recursivo.py
@@ -0,0 +1,11 @@
+
+
+def imprimir_numero_recursivo(numero):
+    if numero > 1:
+        print(numero)
+        impromir_numero_recursivo(numero - 1)
+    elif numero <= 0 :
+        print("Valor incorrecto")
+
+
+imprimir_numero_recursivo(5)
