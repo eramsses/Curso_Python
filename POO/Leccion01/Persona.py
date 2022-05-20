f980de85d5961aa23ca011e622b42c30b707b29f
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2022-05-19 18:56:20 -0600

    Update Persona.py

--- POO/Leccion01/Persona.py
+++ POO/Leccion01/Persona.py
@@ -1,15 +1,14 @@
 class Persona:
 
-
+    #constructor de la clase se llama __init_
     def __init__(self, nombre, apellido, edad):
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
 
+    #Función para imprimir la información de la clase
     def imprimir(self):
-        print(self.nombre)
-        print(self.apellido)
-        print(self.edad)
+        print(f"Nombre: {self.nombre} {self.apellido} {self.edad} años")
 
 persona1 = Persona("Erick", "Rodríguez", 43)
 persona2 = Persona("Alejandra", "Rodríguez", 9)
