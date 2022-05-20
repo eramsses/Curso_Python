cadf0ceb206839ef8e9d7ca9982f14913ca259ac
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2022-05-20 01:21:47 -0600

    Impelentación desde otra clase
    Se impide que las pruebas se ejecuten las pruebas que teníamos

--- POO/Leccion01/Persona.py
+++ POO/Leccion01/Persona.py
@@ -35,21 +35,24 @@ class Persona:
     def mostrar_detalle(self):
         print(f"Nombre: {self.__nombre} {self.__apellido} {self.__edad} años")
 
+#Pruebas que se ejecutan solo si es desde la misma clase Persona
+if __name__ == "__main__":
+    persona1 = Persona("Erick", "Rodríguez", 43)
+    persona2 = Persona("Alejandra", "Rodríguez", 9)
 
-persona1 = Persona("Erick", "Rodríguez", 43)
-persona2 = Persona("Alejandra", "Rodríguez", 9)
+    #Agregando otro atributo de clase desde su uso o dinamicamente,
+    # éste no se comparte con las otras instancias
+    persona1.telefono = "50465445533"
+    persona1.__nombre = "Ramsses" #No es accesible desde afuera de la clase
 
-#Agregando otro atributo de clase desde su uso o dinamicamente,
-# éste no se comparte con las otras instancias
-persona1.telefono = "50465445533"
-persona1.__nombre = "Ramsses" #No es accesible desde afuera de la clase
+    #persona1.mostrar_detalle()
+    Persona.mostrar_detalle(persona1)#otra forma de utilizar los metodos de la clase
+    print(persona1.telefono)
+    persona2.mostrar_detalle()
 
-#persona1.mostrar_detalle()
-Persona.mostrar_detalle(persona1)#otra forma de utilizar los metodos de la clase
-print(persona1.telefono)
-persona2.mostrar_detalle()
+    #Uso encapsulado
+    print(persona1.nombre)
+    persona1.nombre = "Ramsses"
+    print(persona1.nombre)
 
-#Uso encapsulado
-print(persona1.nombre)
-persona1.nombre = "Ramsses"
-print(persona1.nombre)
+    print(__name__)
---
+++ POO/Leccion01/PruebaPersonas.py
@@ -0,0 +1,8 @@
+#Importar la clase Persona
+from Persona import Persona
+
+persona1 = Persona("Karla", "Goméz", 28)
+
+persona1.mostrar_detalle()
+
+
