6182d8b41067e5c55ad8e0529c11cd113082d8d3
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2022-05-20 00:18:12 -0600

    Agregando atributos dinamicamente en la clase

--- POO/Leccion01/Persona.py
+++ POO/Leccion01/Persona.py
@@ -1,18 +1,24 @@
 class Persona:
 
-    #constructor de la clase se llama __init_
+    # constructor de la clase se llama __init_
     def __init__(self, nombre, apellido, edad):
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
 
-    #Función para imprimir la información de la clase
-    def imprimir(self):
+    # Función para imprimir la información de la clase
+    def mostrar_detalle(self):
         print(f"Nombre: {self.nombre} {self.apellido} {self.edad} años")
+
 
 persona1 = Persona("Erick", "Rodríguez", 43)
 persona2 = Persona("Alejandra", "Rodríguez", 9)
 
-persona1.imprimir()
-persona2.imprimir()
+#Agregando otro atributo de clase desde su uso o dinamicamente,
+# éste no se comparte con las otras instancias
+persona1.telefono = "504215643"
 
+#persona1.mostrar_detalle()
+Persona.mostrar_detalle(persona1)#otra forma de utilizar los metodos de la clase
+print(persona1.telefono)
+persona2.mostrar_detalle()
