acceedc929daa0ee56c793cee1657b26e394c8ab
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2022-05-22 16:29:21 -0600

    Mejora de código

--- POO/Leccion06/Persona.py
+++ POO/Leccion06/Persona.py
@@ -2,10 +2,15 @@ class Persona:
     contador_persona = 0
 
     def __init__(self, nombre, edad):
-        Persona.contador_persona += 1
-        self.__id_persona = Persona.contador_persona
+
+        self.__id_persona = self.__generear_siguiente_valor()
         self.__nombre = nombre
         self.__edad = edad
+
+    @classmethod
+    def __generear_siguiente_valor(cls):
+        cls.contador_persona += 1
+        return cls.contador_persona
 
     @property
     def id_persona(self):#Solo lectura
@@ -25,8 +30,11 @@ class Persona:
 
 p1 = Persona("Juan", 38)
 p2 = Persona("Karla", 30)
+
 print(p1)
 print(p2)
+
+print(p1.contador_persona)
 
 print()
 
