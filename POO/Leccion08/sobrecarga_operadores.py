158acb062c0b9ad5354ce49da55c4ec95c2fb4c2
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2022-05-22 17:14:54 -0600

    Sobre carga de operadores

---
+++ POO/Leccion08/sobrecarga_operadores.py
@@ -0,0 +1,22 @@
+
+#Operador +
+
+a = 2
+b = 3
+
+#operación matemática
+print(a + b)
+
+#Concatena cadena
+a = "Hola "
+b = "Mundo"
+print(a + b)
+
+#Unir listas
+a = [1,2,3]
+b = [4,5,6]
+
+print(a + b)
+
+
+
