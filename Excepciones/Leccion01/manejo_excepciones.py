8a8b96d6e6e1e398a8b8dcb735e85ad78fde64d5
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2022-05-22 20:02:20 -0600

    Update manejo_excepciones.py

--- Excepciones/Leccion01/manejo_excepciones.py
+++ Excepciones/Leccion01/manejo_excepciones.py
@@ -1,15 +1,16 @@
 
 resultado = None
-a = 10
-b = 2
+
 try:
+    a = int(input("Primer número: "))
+    b = int(input("Segundo número: "))
     resultado = a / b
 except ZeroDivisionError as ex:
     print(f"Ocurrió el error: {ex}")
 except TypeError as ex:
     print(f"Ocurrió el error: {ex}")
 except Exception as ex:
-    print(f"Ocurrió el error: {ex}")
+    print(f"Ocurrió el error: {ex} - {type(ex)}")
 
-print(f"Resultado: {resultado:.2}")
+print(f"Resultado: {resultado:}")
 print("Continuamos....")
