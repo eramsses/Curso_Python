0cb388d3dbcd186c54a77b48d56e88a8ac66ed4d
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2022-05-25 01:26:40 -0600

    Update transacciones.py

--- BD/transacciones/transacciones.py
+++ BD/transacciones/transacciones.py
@@ -8,5 +8,19 @@ conexion = bd.connect(
     database="test_db")
 
 try:
+    conexion.autocommit = False
     cursor = conexion.cursor()
-    sentencia = "INSERT"
+    sentencia = "INSERT INTO personas(nombre, apellido, email) VALUES(%s, %s, %s)"
+    valores = ("Maria", "Esperanza", "mesperanza@yopmail.com")
+    cursor.execute(sentencia, valores)
+
+    conexion.commit()
+    print(f"Se afectaron: {cursor.rowcount}")
+
+
+except Exception as e:
+    conexion.rollback()
+    print("Se hizo rollback")
+    print(f"ERROR: {e}")
+finally:
+    conexion.close()
