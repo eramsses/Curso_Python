051349acaebaa12887c795bcba600c6703b54b05
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2022-05-25 00:17:35 -0600

    Conexión a base de datos postgresql

---
+++ BD/Leccion01/prueba_db.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+conexion = psycopg2.connect(
+    user="postgres",
+    password="michelle",
+    host="127.0.0.1",
+    port="5432",
+    database="test_db")
+
+
+cursor = conexion.cursor()
+sentencia = "SELECT * FROM personas"
+cursor.execute(sentencia)
+
+registros = cursor.fetchall()
+
+print(registros)
+
+cursor.close()
+conexion.close()
+
+
+
