611500dadc0fca2d07e373361f2ea868b8657c47
Erick R. Rodríguez <47439411+eramsses@users.noreply.github.com>
2022-05-31 19:54:05 -0600

    Retomando el curso
    Después del cambio de SSD

--- BD/Leccion01/prueba_db.py
+++ BD/Leccion01/prueba_db.py
@@ -10,17 +10,17 @@ conexion = psycopg2.connect(
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            #sentencia = "INSERT INTO personas(nombre, apellido, email) VALUES(%s, %s, %s)"
-            #valores = (
-            #    ("Marco", "Flores", "mflores@yopmail.com"),
-            #    ("Angel", "Martinez", "amartinez@yopmail.com"),
-            #    ("Maria", "Gonzalez", "arodriguez@yopmail.com")
-            #)
+            sentencia = "INSERT INTO personas(nombre, apellido, email) VALUES(%s, %s, %s)"
+            valores = (
+                ("Marco", "Flores", "mflores@yopmail.com"),
+                ("Angel", "Martinez", "amartinez@yopmail.com"),
+                ("Maria", "Gonzalez", "arodriguez@yopmail.com")
+            )
 
-            sentencia = "UPDATE personas SET nombre = %s, email = %s WHERE id_persona = %s"
-            valores = ("Juan Carlos", "jalgo@yopmail.com", 13)
-            #cursor.execute(sentencia, valores)
-            cursor.execute(sentencia, valores)
+            #sentencia = "UPDATE personas SET nombre = %s, email = %s WHERE id_persona = %s"
+            #valores = ("Juan Carlos", "jalgo@yopmail.com", 13)
+            ##cursor.execute(sentencia, valores)
+            cursor.executemany(sentencia, valores)
             #conexion.commit() #no se usa si está dentro del with
 
             registros_insertados = cursor.rowcount
